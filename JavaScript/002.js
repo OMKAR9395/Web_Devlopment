402f5f1de574fba4bd44f5d4ca8a16562a2cc20a
OMKAR9395 <omkarmohite85@gmail.com>
2024-10-29 11:31:02 +0530

    js

--- JavaScript/002.js
+++ JavaScript/002.js
@@ -1,2 +1,4 @@
 let str = "Omkar Mohite";
 console.log(str[8]);
+let name = "omkar Patil";
+console.log(name);
---
+++ JavaScript/Array01.js
@@ -0,0 +1,15 @@
+// let heroes = ["ironman" ,"thor","hulk","shaaktiman","batman"];
+
+// for(let i=0;i<heroes.length;i++){
+//     console.log(heroes[i]);
+// }
+// let heroes = ["ironman" ,"thor","hulk","shaaktiman","batman"];
+
+// for(let hero of heroes){
+//     console.log(hero);
+// }
+let heroes = ["ironman" ,"thor","hulk","shaaktiman","batman"];
+
+for(let hero of heroes){
+    console.log(hero.toUpperCase());
+}
