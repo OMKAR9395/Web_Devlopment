214c20fad778e89ba0b19c89a850f40f23af1675
OMKAR9395 <omkarmohite85@gmail.com>
2024-11-01 15:02:28 +0530

    js

--- JavaScript/Dom01.js
+++ JavaScript/Dom01.js
@@ -1,2 +1,5 @@
-console.dir(document.body);
-console.log(document.body);
+// console.dir(document.body);
+// console.log(document.body);
+// document.body.style.background = "green";
+let Heading= document.getElementById("01");
+console.dir(Heading);
