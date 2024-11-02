fa66a4d6dd5d57fab9f184ce4f89c73f968a5076
OMKAR9395 <omkarmohite85@gmail.com>
2024-11-02 20:59:00 +0530

    js

---
+++ JavaScript/Dom.js
@@ -0,0 +1,7 @@
+// let para = document.querySelector("p");
+// console.log(para.setAttribute("id","new"))
+let div = document.querySelector("div");
+
+div.style.backgroundColor = "green";
+div.innerText = "Patil";
+div.style.fontSize = "26px";
