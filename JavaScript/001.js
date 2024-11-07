d985cea1dfbd94f92ffae72c6488f4f4a09c879e
OMKAR9395 <omkarmohite85@gmail.com>
2024-11-07 21:34:38 +0530

    js

--- JavaScript/001.js
+++ JavaScript/001.js
@@ -2,7 +2,6 @@ let gameNum = 25;
 let userNum = prompt("Guess Game Num :");
 
 while(userNum != gameNum) {
-    
     userNum = prompt("You Entered Wrong Number . Guess Again :");
 }
 console.log(" Congratilation You Entered Correct Number");
