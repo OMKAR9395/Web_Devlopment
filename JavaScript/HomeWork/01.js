98317d19f1f687d9a05f1c8d5d4b7d04d7d87407
OMKAR9395 <omkarmohite85@gmail.com>
2024-11-12 11:21:39 +0530

    js

---
+++ JavaScript/HomeWork/01.js
@@ -0,0 +1,6 @@
+let newBtn = document.createElement("button");
+newBtn.innerText = "click me!";
+
+newBtn.style.color="white";
+newBtn.style.backgroundColor ="red";
+document.querySelector("body").prepend(newBtn);
