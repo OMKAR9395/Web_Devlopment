031574aa03ca8ffbb088bdbabc03f8b32add33e9
OMKAR9395 <omkarmohite85@gmail.com>
2024-11-12 11:14:57 +0530

    js

---
+++ JavaScript/Dom04.js
@@ -0,0 +1,19 @@
+let newBtn = document.createElement("button");
+newBtn.innerText = "click me!";
+console.log(newBtn);
+
+let div = document.querySelector("div");
+div.after(newBtn);
+// div.before(newBtn);
+// div.prepend(newBtn);
+// div.append(newBtn);
+
+let newHeading = document.createElement("h1");
+newHeading.innerHTML = "<i>Hi, I  am New!</i>";
+
+document.querySelector("body").prepend(newHeading);
+
+let para = document.querySelector("p");
+para.remove();
+
+
