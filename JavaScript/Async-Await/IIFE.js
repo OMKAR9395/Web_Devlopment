4d65ac0bc17f87c6fb8c10970a9c8465f9505440
OMKAR9395 <omkarmohite85@gmail.com>
2024-11-27 13:15:48 +0530

    js

---
+++ JavaScript/Async-Await/IIFE.js
@@ -0,0 +1,25 @@
+(async function () {
+    console.log("Getting Data 1....");
+    await getData(1);
+    console.log("Getting Data 2....");
+    await getData(2);
+    console.log("Getting Data 3....");
+    await getData(3);
+    console.log("Getting Data 4....");
+    await getData(4);
+    console.log("Getting Data 5....");
+    await getData(5);
+    console.log("Getting Data 6....");
+    await getData(6);
+})();
+
+
+
+function getData(dataId){
+    return new Promise((resolve, reject) => {
+        setTimeout(()=>{
+            console.log("data", dataId);
+            resolve("success");
+        },2000);
+    });
+}
---
+++ JavaScript/Async-Await/app.js
@@ -0,0 +1,20 @@
+
+
+function getData(dataId){
+    return new Promise((resolve, reject) => {
+        setTimeout(()=>{
+            console.log("data", dataId);
+            resolve("success");
+        },2000);
+    });
+}
+
+async function getAllData() {
+    await getData(1);
+    await getData(2);
+    await getData(3);
+    await getData(4);
+    await getData(5);
+    await getData(6);
+
+}
