7f5779fe94297ca3b3aa65ade76415be960f3352
OMKAR9395 <omkarmohite85@gmail.com>
2024-12-01 23:01:20 +0530

    js

--- JavaScript/Async-Await/IIFE.js
+++ JavaScript/Async-Await/IIFE.js
@@ -23,9 +23,10 @@
 function getData(dataId){
     return new Promise((resolve, reject) => {
         setTimeout(()=>{
-            
+
             console.log("data", dataId);
             resolve("success");
+            
         },2000);
     });
 }
