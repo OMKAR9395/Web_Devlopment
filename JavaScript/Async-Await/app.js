1bf41f8dc47b7af2229a8be4c8840256537faf39
OMKAR9395 <omkarmohite85@gmail.com>
2024-12-02 21:34:49 +0530

    js

--- JavaScript/Async-Await/app.js
+++ JavaScript/Async-Await/app.js
@@ -13,9 +13,10 @@ async function getAllData() {
     await getData(1);
 
     await getData(2);
-    
+
     await getData(3);
     await getData(4);
+    
     await getData(5);
     await getData(6);
 
