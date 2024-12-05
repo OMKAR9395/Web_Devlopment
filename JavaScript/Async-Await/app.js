2ed3ac272119f6b1802a722d1434c0dc30bbcada
OMKAR9395 <omkarmohite85@gmail.com>
2024-12-05 19:35:46 +0530

    js

--- JavaScript/Async-Await/app.js
+++ JavaScript/Async-Await/app.js
@@ -3,10 +3,11 @@
 function getData(dataId){
 
     return new Promise((resolve, reject) => {
+        
         setTimeout(()=>{
 
             console.log("data", dataId);
-            
+
             resolve("success");
             
         },2000);
