06c124a02c4650ec55fbf53fbe5315c30fd04099
OMKAR9395 <omkarmohite85@gmail.com>
2024-12-18 22:06:21 +0530

    js

--- JavaScript/API/app.js
+++ JavaScript/API/app.js
@@ -3,6 +3,7 @@ const URL = "https://cat-fact.herokuapp.com/facts";
 
 
 const getFacts = async () => {
+    
     console.log("GettingData....");
     let response = await fetch(URL);
     console.log(response);
