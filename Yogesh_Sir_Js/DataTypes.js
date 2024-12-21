fdc7ce93111f193e697571413fb5ffaa8624715d
OMKAR9395 <omkarmohite85@gmail.com>
2024-12-21 12:21:01 +0530

    Js

---
+++ Yogesh_Sir_Js/DataTypes.js
@@ -0,0 +1,65 @@
+/**
+    Primitive Data Type
+
+1.  Number (1,1.25,0.251,1254)
+2.  String  ("omkar",'patil',"",'',"024")
+3.  BigInt (var x = Bigint("2565652565652"))
+4.  Boolean (True False)
+5.  Undefined
+6.  Null
+
+ */
+
+/*
+
+let MyDemo = "Hello" - "World";
+console.log(MyDemo);
+console.log(typeof(MyDemo));
+
+let x = 0;
+console.log(isNaN(x));
+console.log(typeof(x));
+
+*/
+
+/*
+var x = true - false;
+console.log(x);
+
+const d = [10,20,30];
+const f = {data:"omkar",age:20,RollNo:121};
+console.log(typeof(d));
+console.log(typeof(f));
+*/
+
+/*      Array literal And Array New Keyword Use
+const MyArr = new Array(10);    //new keyword use array
+const MyArr01 = new Array(15,10,12,13,41);
+console.log(MyArr);
+console.log(MyArr01);
+*/
+//////////////////////////////////////////////////////
+
+/*
+            Array Length
+var MyArr = ['web Developmment',true,121,500,'Hello'];
+console.log(MyArr.length);
+
+
+const StudentMarks = [12, 12, 20, 12, 23, 50];
+StudentMarks[StudentMarks.length] = 24;
+const LastIndex = StudentMarks.length - 1;
+console.log(LastIndex);
+console.log(StudentMarks[LastIndex]);
+
+
+
+*/
+/*
+    Object
+*/
+
+const StudentInformation = {Name:"Omkar",Address:"Masur",Roll_No:121,Grade:true};
+console.log(StudentInformation);
+console.log(StudentInformation.Name);
+console.log(StudentInformation.Address,StudentInformation.Roll_No);
---
+++ Yogesh_Sir_Js/Operators.js
@@ -0,0 +1,56 @@
+/*
+1.  Arithmatic Operator
+    (+ ,- ,* ,/ ,% ,** ,++ ,--)
+Power **
+var x = 10;
+var y = 3;
+console.log(x ** y);
+console.log(Math.pow(10,2));
+
+        Increment 
+    Post Increment 
+let x = 20;
+console.log(x++);//20
+let y = x++;
+console.log(y);//21
+console.log(x);//22
+
+    Pre Increment
+    let x = 20;
+console.log(++x);//21
+let y = ++x;
+console.log(y);//22
+console.log(x);//22
+*/
+/*
+
+ 2. Comparison Operator
+(==,!=,===,>,<,>=,<=,?,!==)
+
+Strictly Equality
+
+var x=10;
+var y="10";
+console.log(x==y);
+console.log(x===y);
+
+Strictly Not Equality
+
+console.log(1!==1); //False
+console.log("1"!==1);   //true
+console.log(0!==false); //true
+console.log("true"!==1);    //true
+*/
+
+/*
+    3. Assignment Operator
+    (=,+=,-=,/=,*=,**=)
+
+    4.Logical Operator
+    (&& , || , !)
+
+    5.Bitwise Operator
+    (&,^,>>,<<,|,~,>>>)
+*/
+
+
