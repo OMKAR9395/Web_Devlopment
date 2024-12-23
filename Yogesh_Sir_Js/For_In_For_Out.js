d44e1c35d51b9b94c0ac21f5db4167038e1a50de
OMKAR9395 <omkarmohite85@gmail.com>
2024-12-23 15:28:18 +0530

    js

--- Yogesh_Sir_Js/For_In_For_Out.js
+++ Yogesh_Sir_Js/For_In_For_Out.js
@@ -13,8 +13,8 @@
 */
 /*
     for of loop
-*/
-const StudName = ["Test01","Test02","Test03","Test04"];
+
+    const StudName = ["Test01","Test02","Test03","Test04"];
 for(let Names of StudName){
     console.log(Names);
 }
@@ -22,3 +22,70 @@ const Object = {p:1,q:2,r:3,s:4,t:5};
 for(let ObjectData of Object){
     console.log(ObjectData);
 }
+    let strName = "WebDevelopment";
+for(let Name of strName)
+{
+    console.log(Name);
+}
+
+*/
+
+/*
+        String
+
+
+var arr = [10,20,30,40,50,60];
+const y = arr.toString();
+console.log(y);
+ 
+const s = arr.join(" ");
+console.log(s);
+const p = arr.join("+");
+console.log(p);
+
+var StudName = ["Omkar","Demo","Fork","Web"];
+StudName.sort();
+console.log(StudName.sort());
+
+*/
+
+//      Array Element Crud App
+
+/*
+var arr = [10,20,30,40,50];
+arr.push(60);   //Insert Last Element
+console.log(arr);
+arr.pop();  //Delet Last Element
+console.log(arr);
+arr.shift();    //Remove First Element
+console.log(arr);
+arr.unshift(10);
+console.log(arr);   //Add Element First
+
+let Name = ["omkar","fork","patil"];
+Name.reverse();     //Reverse Order
+console.log(Name);
+console.log(Array.isArray(Name));   //True Or False Array Or Not
+
+
+        Array 
+
+let Arr = ["Omkar","Patil","Web"];
+let ptr = ["Fork","Infosystem","Development"];
+
+const x = Arr.concat(ptr);
+console.log(x);
+*/
+
+//          Slice
+//          Not Modify Original Array
+//          ArrName.slice(begin,end);
+//          ArrName.slice(1,3)  Not Consider a Last Element
+//          Start With Indexing
+// var value = ["Fork","Infosystem","Development","Omkar","Patil","Web"];
+// let x = value.slice(1,4);
+// console.log(x);
+
+var arr = [10,20,30,40,50,60];
+const y = arr.toString();
+console.log(y);
