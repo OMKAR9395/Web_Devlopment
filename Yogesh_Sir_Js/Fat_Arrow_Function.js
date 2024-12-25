1d5ab63ed8758e020a5b41cfb352857b0b1eea64
OMKAR9395 <omkarmohite85@gmail.com>
2024-12-25 13:36:43 +0530

    js

---
+++ Yogesh_Sir_Js/Fat_Arrow_Function.js
@@ -0,0 +1,13 @@
+//      Fat Arrow Function
+
+const Sum = ()=>{
+    let a=10;
+    let b=20;
+    return a+b;
+};
+console.log(Sum());
+
+let x = 30;
+let y = 50;
+const Sum01 = () => x + y;
+console.log(Sum01());
