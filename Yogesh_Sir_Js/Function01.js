c3a97670cc1ae9119e3f4589a691e59866e73368
OMKAR9395 <omkarmohite85@gmail.com>
2024-12-25 13:14:19 +0530

    Js

--- Yogesh_Sir_Js/Function01.js
+++ Yogesh_Sir_Js/Function01.js
@@ -1,4 +1,41 @@
-const Myfunction = function(){
-    console.log("Hello This Is Annonimous Funnction");
+// const Myfunction = function(){
+//     console.log("Hello This Is Annonimous Funnction");
+// }
+// Myfunction();
+
+/*
+
+let x = [10,20,30,40,50];
+const Myfunction = function(Arr){
+    var Sum = 0;
+    for(let i = 0;i<Arr.length;i++){
+        Sum +=Arr[i];
+    }
+    return Sum;
+};
+
+let  Result = Myfunction(x);
+console.log(Result);
+
+*/
+
+//      Function With In Function 
+
+/*
+
+function Cube(x){
+    return x*x*x;
 }
-Myfunction();
+const Result = function(Cube,Value){
+    let NewArr = new Array(Value.length);
+    for(let i = 0 ; i < Value.length;i++){
+        NewArr[i] = Cube(Value[i]);
+    }
+    return NewArr;
+}
+let Value = [10,20,30,40,50];
+let Ret = Result(Cube,Value);
+console.log(Ret);
+
+*/
+
