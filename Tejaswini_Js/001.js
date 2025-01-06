7b0db10af36337b8be306b30c7e282d9a2ad94b9
OMKAR9395 <omkarmohite85@gmail.com>
2025-01-06 08:52:31 +0530

    Operetors

---
+++ Tejaswini_Js/001.js
@@ -0,0 +1,48 @@
+// console.log("Hello Teju");
+
+// var x =10;
+// console.log(x);
+
+// var x = "Fork";
+// console.log(x);
+
+// let y = 20;
+// console.log(y);
+
+// let y = "Fork";
+
+// Const 
+
+// const x = 10;
+// x = 20;
+// console.log(x);
+
+
+//  Array
+// const x = [10,20,30,40];
+// console.log(x);
+
+// x[2]=200;
+// console.log(x);
+
+//  object
+
+// const x ={Name:"Fork",Age:23,Address:"Karad"};
+// console.log(x);
+
+// x.Name="Web  Development";
+// console.log(x);
+
+//   data type
+
+// let x = [10,20,30,40];
+// let y ={Name:"Fork",Age:23,Address:"Karad"};
+
+// console.log(typeof(x));
+// console.log(typeof(y));
+
+// x = 10;
+// x++;
+// console.log(x);//11
+// console.log(++x);//11
+// console.log(x);//11
